0681b8a547c59231536a886dd448667b7e270596
sapiens12 <sapiens12@nate.com>
2022-11-03 14:00:14 +0100

    commit image uploader

--- src/index.js
+++ src/index.js
@@ -3,13 +3,21 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './app';
 import AuthService from './service/auth_service';
+import ImageUploader from './service/image_uploader';
+import ImageFileInput from './components/image_file_input/image_file_input';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 const authService = new AuthService();
+const imageUploader = new ImageUploader();
+const FileInput = props => (
+  <ImageFileInput {...props} imageUploader={imageUploader} />
+)
+
+
 root.render(
   <React.StrictMode>
-    <App authService={authService}/>
+    <App authService={authService} FileInput={FileInput}/>
   </React.StrictMode>
 );
 
